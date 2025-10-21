0e59087187190fcba75fc162cd0db2c3a00ca43b
Yertugan <muslim.ipro@gmail.com>
2025-10-21 21:33:12 +0900

    customer relations manager added

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -6,6 +6,7 @@ import emailjs from '@emailjs/browser';
 interface ContactInfo {
     email: string;
     phone: string;
+    phone2: string;
 }
 
 const ContactForm = () => {
@@ -18,7 +19,8 @@ const ContactForm = () => {
     });
     const [contactInfo, setContactInfo] = useState<ContactInfo>({
         email: '',
-        phone: ''
+        phone: '',
+        phone2: '',
     });
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [isSubmitted, setIsSubmitted] = useState(false);
@@ -164,6 +166,22 @@ const ContactForm = () => {
                     <a href={`tel:${contactInfo.phone}`} className="text-primary hover:underline">
                         {contactInfo.phone}
                     </a>
+                </div>
+                <div className='flex flex-col'>
+
+                    <label htmlFor="name" className="block text-sm font-medium text-gray-700 pl-2">
+                            {t('customerSupportManager')}
+                        </label>
+                <div className="flex items-center gap-3">
+                    <div className="p-2 bg-primary/10 rounded-lg text-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                        </svg>
+                    </div>
+                    <a href={`tel:${contactInfo.phone2}`} className="text-primary hover:underline">
+                        {contactInfo.phone2}
+                    </a>
+                </div>
                 </div>
             </div>
 
